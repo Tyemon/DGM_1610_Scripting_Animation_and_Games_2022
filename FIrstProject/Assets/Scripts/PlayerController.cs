6c3d30e473e0c9f7e6fc439faed9fecfaccca41c
Ty Hendricksen <Tyscottmon@gmail.com>
2022-08-30 09:19:15 -0600

    scripting and such

--- FIrstProject/Assets/Scripts/PlayerController.cs
+++ FIrstProject/Assets/Scripts/PlayerController.cs
@@ -2,23 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
 //Set speed
-    public int speed;
+    public float speed;
+    // rotation speed
+    public float rotSpeed;
     //horizontal input
     public float hInput;
     //vertical input
     public float vInput;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
+
 
     // Update is called once per frame
     void Update()
     {
+        //collect input values from keyboard
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+        //move character
+        transform.Rotate(Vector3.up, rotSpeed * hInput * Time.deltaTime); // left and right
         
+        transform.Translate(Vector3.forward * speed * vInput * Time.deltaTime); //Forward
     }
 }
